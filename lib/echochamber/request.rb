5316a7a3262ac6958a55c1bb2b3cd9169820420d
Bernard Worthy <worthybernard@gmail.com>
2016-04-01 11:09:39 -0400

    changing header in get refresh request

--- lib/echochamber/request.rb
+++ lib/echochamber/request.rb
@@ -43,7 +43,7 @@ module Echochamber::Request
   # @param credentials [Echochamber::Credentials] Initialized Echochamber::Credentials
   # @return [String] Valid authentication token
   def self.get_token_from_refresh(credentials)
-    headers = { :content_type => :json, :accept => :json  }
+    headers = {}
     response = post(ENDPOINT.fetch(:refresh), credentials, headers)
     response_body = JSON.parse(response.body)
     response_body.fetch("accessToken")
