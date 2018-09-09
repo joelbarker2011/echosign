wss5s/echochamber

Update agreement to use recipient set since the api seems to have changed

lib/echochamber/agreement/recipient_set.rb
@@ -1,0 +1,20 @@
+module Echochamber
+  class RecipientSet < Hash
+
+    include Validatable
+
+    # Creates an Echochamber::RecipientSet object 
+    #
+    # @param [Hash] params SYMBOL-referenced Hash.  Role is required for the set. Either fax or email is required for each recipient member.
+    # @option params [String] :recipientSetRole ['SIGNER' or 'APPROVER']: Specify the role of recipient set (REQUIRED)
+    # @option params [Array] :recipientSetMemberInfos A list of emails of the recipients.  Populate with array of isntances of {Echochamber::RecipientSetMember Echochamber::RecipientSetMember}(REQUIRED)
+    # @return [Echochamber::RecipientSet]
+
+    def initialize(params)
+      require_keys([:recipientSetRole, :recipientSetMemberInfos], params)
+      merge!(params)
+    end
+
+  end
+end
+

lib/echochamber/agreement/recipient_set_member.rb
@@ -1,0 +1,19 @@
+module Echochamber
+  class RecipientSetMember < Hash
+
+    include Validatable
+
+    # Creates an Echochamber::RecipientSetMember object 
+    #
+    # @param [Hash] params SYMBOL-referenced Hash.  Email is required for each recipient set member.
+    # @option params [String] :email Emails of the recipient.(REQUIRED)
+    # @return [Echochamber::RecipientSetMember]
+
+    def initialize(params)
+      require_keys([:email], params)
+      merge!(params)
+    end
+
+  end
+end
+

joelbarker2011/echosign

lib/echosign/agreement/recipient_set.rb
@@ -1,0 +1,20 @@
+module Echosign
+  class RecipientSet < Hash
+
+    include Validatable
+
+    # Creates an Echosign::RecipientSet object 
+    #
+    # @param [Hash] params SYMBOL-referenced Hash.  Role is required for the set. Either fax or email is required for each recipient member.
+    # @option params [String] :recipientSetRole ['SIGNER' or 'APPROVER']: Specify the role of recipient set (REQUIRED)
+    # @option params [Array] :recipientSetMemberInfos A list of emails of the recipients.  Populate with array of isntances of {Echosign::RecipientSetMember Echosign::RecipientSetMember}(REQUIRED)
+    # @return [Echosign::RecipientSet]
+
+    def initialize(params)
+      require_keys([:recipientSetRole, :recipientSetMemberInfos], params)
+      merge!(params)
+    end
+
+  end
+end
+

lib/echosign/agreement/recipient_set_member.rb
@@ -1,0 +1,19 @@
+module Echosign
+  class RecipientSetMember < Hash
+
+    include Validatable
+
+    # Creates an Echosign::RecipientSetMember object 
+    #
+    # @param [Hash] params SYMBOL-referenced Hash.  Email is required for each recipient set member.
+    # @option params [String] :email Emails of the recipient.(REQUIRED)
+    # @return [Echosign::RecipientSetMember]
+
+    def initialize(params)
+      require_keys([:email], params)
+      merge!(params)
+    end
+
+  end
+end
+